5c85e6082ef4cc9727668a594f8b53b205696a4c
johndonneUZH <matteoiulian.adam@uzh.ch>
2025-10-30 19:46:22 +0100

    [SETUP] Added spring initalizr project

---
+++ src/main/java/ch/helpos/backend/HelpOsBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.helpos.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpOsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpOsBackendApplication.class, args);
+	}
+
+}
